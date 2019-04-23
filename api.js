db0fabcd784387d29c88bbe43f380677781ea042
jamesliudotcc <james@jamesliu.cc>
2019-04-22 20:36:11 -0700

    up to listing 2.5

--- api.js
+++ api.js
@@ -3,8 +3,22 @@
 const Api = require('claudia-api-builder');
 const api = new Api();
 
-api.get('/pizzas', () => {
-  return ['Capricciosa', 'Quattro Formaggi', 'Napoletana', 'Margherita'];
+const getPizzas = require('./handlers/get-pizzas');
+
+api.get('/', () => {
+  return 'Pizza api. Use the /pizzas route';
 });
+
+api.get('/pizzas', () => {
+  return getPizzas();
+});
+
+api.get(
+  '/pizzas/{id}',
+  request => {
+    return getPizzas(request.pathParams.id);
+  },
+  { error: 404 }
+);
 
 module.exports = api;
